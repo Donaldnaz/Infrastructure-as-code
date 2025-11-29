7a428a029aee54ebb00aafab2afe16c856206a3f
Anasieze Ikenna <152716902+Donaldnaz@users.noreply.github.com>
2025-11-29 15:32:28 -0330

    Delete Keypair.tf

--- Keypair.tf
+++
@@ -1,8 +0,0 @@
-# Keypair Access
-
-resource "aws_key_pair" "ikenna" {
-
-  key_name   = "ikenna"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0/brj+K77fW1NZVorNtZty92JOwz8JpgliioQ3CihV apple@Champ.local"
-
-}

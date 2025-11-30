e0420924c4ee785ba6114b7cde65b0349bf76ac0
Anasieze Ikenna <152716902+Donaldnaz@users.noreply.github.com>
2025-11-30 16:26:41 -0330

    Updated

---
+++ code/EC2_Inst.tf
@@ -0,0 +1,14 @@
+# EC2 Instance
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ikenna.id
+  instance_type          = "t3.micro"
+  key_name               = "ikenna"
+  vpc_security_group_ids = [aws_security_group.secgroup.id]
+  availability_zone      = "us-east-2a"
+
+  tags = {
+    Name    = "IAC_Instance"
+    Project = "IAC"
+  }
+}

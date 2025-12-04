e973e3efc20791de424d9fbf2296db13f2e47147
Anasieze Ikenna <152716902+Donaldnaz@users.noreply.github.com>
2025-12-04 06:54:21 -0330

    backend.tf

--- code/backend.tf
+++ code/backend.tf
@@ -1,7 +1,9 @@
+# Backend Storage
+
 terraform {
 	backend "s3"
 
-	bucket = "terraform3020"
-	key  = "terraform/backend"
-	region = "us-east-2"
+	bucket  =  "terraform3020"
+	key     =  "terraform/backend"
+	region  =  "us-east-2"
 }

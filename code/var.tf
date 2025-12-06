cda001a347eded965f2a552705027dd6e29fafb2
Anasieze Ikenna <152716902+Donaldnaz@users.noreply.github.com>
2025-12-06 13:50:13 -0330

    var.tf

---
+++ code/var.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  type        = string
+  description = "Region for the resource."
+}
+
+variable "location" {
+  type        = string
+  description = "zone for the resource."
+}
+
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
